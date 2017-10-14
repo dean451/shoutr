69a3a07e0acdeebb2ebd911b162ac63ca2b25c8d
Dean Palm <dean@sxsw.com>
2017-10-13 21:05:16 -0500

    shouts created in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Shouts.create("user_id"=>2, "content_type"=>"TextShout", "content_id"=>3)
+Shouts.create("user_id"=>2, "content_type"=>"TextShout", "content_id"=>2)
+Shouts.create("user_id"=>2, "content_type"=>"TextShout", "content_id"=>1)
